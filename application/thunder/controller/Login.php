aed3246539db9974d9bec9cb0d50a91ea2a185a9
meetchen <17408070327@stu.xust.edu.cn>
2019-11-09 19:56:51 +0800

    for login

--- application/thunder/controller/Login.php
+++ application/thunder/controller/Login.php
@@ -4,9 +4,16 @@ namespace app\thunder\controller;
 
 use think\Controller;
 use think\db;
+use think\Exception;
 
 class Login extends Controller
 {
+    /**
+     * 登陆逻辑
+     * 成功0
+     * 失败1
+     * @return int
+     */
     public function login()
     {
         $user = input("user");
@@ -20,24 +27,33 @@ class Login extends Controller
             return 1;
     }
 
+    /**
+     * 注册逻辑
+     * -1 用户名重复
+     *  0 添加成功
+     * 1 添加失败 未知错误
+     * @return int
+     * @throws \think\exception\DbException
+     * @throws db\exception\DataNotFoundException
+     * @throws db\exception\ModelNotFoundException
+     */
     public function register()
     {
-        /**
-         *  -1 用户名重复
-         *  0 添加成功
-         *  1 添加失败 未知错误
-         */
         $user = input("user");
         $pwd = input("pwd");
         if ($user == null || $pwd == null)
             return 1;
         $data = ['User_user' => $user, 'User_pwd' => $pwd];
-        if (db::name('thunder_user')->where('User_user', $user)->find() != null)
-            return -1;
-        // 在注册的同时返回自增的主键 从而去 创建用户对于的分组
-        if (db::name('thunder_group')->insert(['thunder_user' => db::name('thunder_user')->insertGetId($data)]) == 1)
-            return 0;
-        else
+        try{
+            if (db::name('thunder_user')->where('User_user', $user)->find() != null)
+                return -1;
+            // 在注册的同时返回自增的主键 从而去 创建用户对于的分组
+            if (db::name('thunder_group')->insert(['thunder_user' => db::name('thunder_user')->insertGetId($data)]) == 1)
+                return 0;
+            else
+                return 1;
+        }catch (Exception $exception){
             return 1;
+        }
     }
 }
